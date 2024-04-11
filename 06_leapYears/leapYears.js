8d7c4b156e72770d3612ba26194aa1407f5b9689
Kyle <kylekmatthew15@gmail.com>
2024-04-11 07:29:03 -0400

    Finsihed the leapYears exercise

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,6 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    //divisible by four and 400 but not 100
+    return year % 4 === 0 && (year % 400 === 0 || year % 100 !== 0)
 };
 
 // Do not edit below this line
