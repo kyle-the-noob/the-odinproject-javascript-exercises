1268bb3df98d0e35a72547666eb6af30784423df
Kyle <kylekmatthew15@gmail.com>
2024-04-10 21:31:52 -0400

    Finished the reverseString exercise I used the internet not proud of it

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,10 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    /*let splitString = string.split("");
+    let reverseArray = splitString.reverse();
+    let joinArray = reverseArray.join(""); */
 
-};
+    return string.split("").reverse().join("");
+}
 
 // Do not edit below this line
 module.exports = reverseString;
