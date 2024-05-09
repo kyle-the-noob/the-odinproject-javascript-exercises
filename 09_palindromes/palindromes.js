78015be98f5cba775441b63417ebe24a71ba941c
Kylo-the-noob <kylekmatthew15@gmail.com>
2024-05-08 22:11:56 -0400

    Finished the Palindromes exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,18 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    letters = 'abcdefghijklmnopqurtuvwxyz123456789'
 
+    let filteredString = string.toLowerCase()
+    .split("")
+    .filter(char => letters.includes(char))
+    .join("");
+
+    let reversedString = filteredString.split("").reverse().join("");
+
+    if(filteredString === reversedString) {
+        return true
+    } else {
+        return false
+    }
 };
 
 // Do not edit below this line
