6f8e1f286e5fd63170b8ecde4a75c48cf86afe3b
Kylo-the-noob <kylekmatthew15@gmail.com>
2024-05-09 00:21:31 -0400

    Finally finished the findTheOldest exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,19 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+    let age = array.reduce((lastPerson, curPerson) => {
+        let lastAge = getAge(lastPerson.yearOfBirth, lastPerson.yearOfDeath)
+        let curAge = getAge(curPerson.yearOfBirth, curPerson.yearOfDeath)
 
+        return lastAge < curAge ? curPerson : lastPerson;
+    })
+    return age;
 };
+
+    const getAge = function (birth, death) {
+        if(!death){
+            death = new Date().getFullYear();
+        }
+        return death - birth;
+    }
 
 // Do not edit below this line
 module.exports = findTheOldest;
