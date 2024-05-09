b792bb055d43ed668b3fa21403c75b4bf27a45db
Kylo-the-noob <kylekmatthew15@gmail.com>
2024-05-08 22:12:30 -0400

    Finished the fibonacci exericse

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    let a = 0;
+    let b = 1;
+    num = +(num);
+    if (num === 0) {
+        return 0
+    } else if (num < 0){
+        return "OOPS";
+    } else {
+        for(let i = 0; i < (num -1); i++){
+            let c = a + b;
+            a = b;
+            b = c;
+        }
+        return b;
+    }
 };
 
 // Do not edit below this line
