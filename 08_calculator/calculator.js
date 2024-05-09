88c16cbb4a33a7cff869522a721e8dc657dc84e2
Kylo-the-noob <kylekmatthew15@gmail.com>
2024-05-08 22:10:50 -0400

    Finished the calculator exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,35 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(arrays) {
+  let arraySum = arrays.reduce((a, b) => a + b, 0);
+return arraySum;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let multiArr = array.reduce((a, b) => a * b);
+  return multiArr
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+  return num1 ** num2;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if (num == 0) {
+    return 1
+  } else {
+  let result = 1;
+    while (num > 1) {
+      result *= num;
+      num--;
+    }
+    return result;}
 };
 
 // Do not edit below this line
